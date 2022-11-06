03c3197e38d5f2be39144d156fd43739da142e8c
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-11-06 23:39:37 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients');
+
+const items = ingredients.map(ingredient => {
+const itemEl = document.createElement('li');
+itemEl.textContent = `${ingredient}`;
+itemEl.classList.add('item');
+
+return itemEl;
+});
+
+listEl.append(...items);
+

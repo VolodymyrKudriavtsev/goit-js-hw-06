4001b3d543c0b3ffc8b5547e59a6b020d51b18de
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-11-07 01:32:27 +0200

    task 01 & 03 (in progress)

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,12 @@
-const itemsRef = document.querySelectorAll('.item');
-console.log(`Number of categories: ${itemsRef.length}`);
+const itemsRef = document.querySelectorAll(".item");
+console.log("Number of categories: ", itemsRef.length);
 
 itemsRef.forEach(item => {
-  const title = item.firstElementChild.textContent;
-  const elementsRef = item.querySelectorAll('li');
-  
-console.log(`Category: ${title}`);
-console.log(`Elements: ${elementsRef.length}`);
+	console.log("Category: ", item.firstElementChild.textContent);
+	console.log("Elements: ", item.lastElementChild.children.length);
+	// const title = item.firstElementChild.textContent;
+	const elementsRef = item.querySelectorAll("li");
+
+	// console.log(`Category: ${title}`);
+	// console.log(`Elements: ${elementsRef.length}`);
 });

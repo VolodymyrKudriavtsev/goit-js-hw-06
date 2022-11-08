02c00f92eed17f7b8e2b3a48c51f28cfbbe1b7af
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-11-09 01:42:23 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
+};
+
+const onBtnClick = e => {
+  const colorValue = getRandomHexColor();
+  console.log(colorValue);
+
+  document.body.style.backgroundColor = colorValue;
+  refs.color.textContent = colorValue;
+};
+
+refs.button.addEventListener('click', onBtnClick);

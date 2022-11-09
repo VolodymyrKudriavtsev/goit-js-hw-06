104c2bc6bbc2765cc6a781b8d4f9f87ea4da903f
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-11-09 22:52:46 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,20 +2,15 @@ function getRandomHexColor() {
 	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function createBoxes (amount) {
+function createBoxes(amount) {
 	const divsArrey = [];
 	let size = 20;
 	for (let i = 0; i < amount; i += 1) {
-		const box = () => {
-			size += 10;
-			return `<div style='background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px'></div>`;
-		};
-
-		divsArrey.push(box());
+		size += 10;
+		divsArrey.push(`<div style='background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px'></div>`);
 	}
-	
 	refs.divBoxes.insertAdjacentHTML("afterbegin", divsArrey.join(""));
-};
+}
 
 const refs = {
 	divBoxes: document.querySelector("#boxes"),
@@ -25,9 +20,9 @@ const refs = {
 	amount: 0,
 };
 
-const onInputChange = (e) => refs.amount = Number(e.currentTarget.value);
+const onInputChange = e => (refs.amount = Number(e.currentTarget.value));
 const onCreateBtnClick = () => createBoxes(refs.amount);
-const onDestroyBtnClick = () => refs.divBoxes.innerHTML = '';
+const onDestroyBtnClick = () => (refs.divBoxes.innerHTML = "");
 
 refs.inputValue.addEventListener("change", onInputChange);
 refs.createBtn.addEventListener("click", onCreateBtnClick);
